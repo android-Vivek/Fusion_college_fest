33b2ed1dde01b5efc8c4e227dc0990ed45d18393
vivek <vivekmaheshsahu@gmail.com>
2018-02-09 01:40:54 +0530

    Base created splash screen and menu item is created

---
+++ app/src/main/java/technical/test/system/fusion/splashScreen/Isplash.java
@@ -0,0 +1,10 @@
+package technical.test.system.fusion.splashScreen;
+
+/**
+ * Created by Hack on 2/9/2018.
+ */
+
+public interface Isplash {
+
+    void finishAnimation();
+}
---
+++ app/src/main/java/technical/test/system/fusion/splashScreen/Splash.java
@@ -0,0 +1,45 @@
+package technical.test.system.fusion.splashScreen;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+import technical.test.system.fusion.mainActivity.MainActivity;
+import technical.test.system.fusion.R;
+
+public class Splash extends Activity implements Isplash   {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+        ImageView imageView= (ImageView) findViewById(R.id.imageView);
+        Animation an= AnimationUtils.loadAnimation(this,R.anim.topdown);
+        imageView.startAnimation(an);
+        an.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+            }
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                finish();
+               finishAnimation();
+            }
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+            }
+        });
+
+
+    }
+
+    @Override
+    public void finishAnimation() {
+        Intent intent=new Intent(Splash.this,MainActivity.class);
+        startActivity(intent);
+    }
+}
+
