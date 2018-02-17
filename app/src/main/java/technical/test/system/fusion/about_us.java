f0bed78525463d44ad0739b5e037618f07bed888
vivek <vivekmaheshsahu@gmail.com>
2018-02-17 12:45:37 +0530

    APP version 1.0 release

---
+++ app/src/main/java/technical/test/system/fusion/about_us.java
@@ -0,0 +1,13 @@
+package technical.test.system.fusion;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class about_us extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_us);
+    }
+}
